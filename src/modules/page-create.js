2a26bd4cb5f7df1f3a822bf159cfdd39393c8bcd
jorgejaimesg <158189835+Jorgejaimesg@users.noreply.github.com.>
2024-05-29 08:59:46 -0500

    feat :sparkles: we reestructure the components

--- src/modules/page-create.js
+++ src/modules/page-create.js
@@ -32,15 +32,27 @@ export class pageCreate extends LitElement {
             } else {
             return html`
             <div class="form__group field">
-                <input type="input" class="form__field" placeholder="${key}" required="" id="${key}" name="${key}">
+                <input type="input" class="form__field" placeholder="${item}" required="" id="${key}" name="${key}">
                 <label for="${key}" class="form__label"></label>
             </div>
             `}
         }
         )}
         </div>
+        <a class="back-button">Go back</a>
         `
     }
+    firstUpdated(){
+        
+        const backbutton=this.shadowRoot.querySelector('.back-button')
+        backbutton.addEventListener('click',()=>{
+            const principalPage=`<principal-pages></principal-pages>`;
+            this.parentNode.insertAdjacentHTML('beforeend',principalPage);
+            this.parentNode.removeChild(this);
+        })
+    }
+
 }
+
 
 customElements.define("page-create", pageCreate);
