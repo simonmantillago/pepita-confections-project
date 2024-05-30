49108d19e0fef55ba886edc6168d4fcf93f8b006
AndreyJz <jerez.01.18@gmail.com>
2024-05-29 21:59:52 -0500

    feat: :sparkles: search-bar finished

--- src/modules/page-search.js
+++ src/modules/page-search.js
@@ -9,17 +9,20 @@ export class pageSearch extends LitElement{
         this.type = pageData.type
         this.department = pageData.option
         this.Modified = {}
-        this.tag
+        this.tag = ''
+        this.searchItem = ''
+        this.requestUpdate();
     }
-
+    
     async fetchData() {
         try {
-          const response = await fetch(`https://66560fd13c1d3b60293c1866.mockapi.io/${this.type}`);
-          const data = await response.json();
-          this.data = data;
-          console.log(data[0])
+            const response = await fetch(`https://6657bc775c3617052645bc99.mockapi.io/Inventory`);
+            const data = await response.json();
+            this.data = data;
+            console.log(this.data);
+            this.requestUpdate();
         } catch (error) {
-          console.error('Error fetching data:', error);
+            console.error('Error fetching data:', error);
         }
     }
 
@@ -27,7 +30,7 @@ export class pageSearch extends LitElement{
         if (!this.editItem) return;
 
         try {
-            const response = await fetch(`https://664b70de35bbda10987cf5f7.mockapi.io/people/${this.editItem.id}`, {
+            const response = await fetch(`https://664b70de35bbda10987cf5f7.mockapi.io/Inventory/${this.editItem.id}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(this.editItem)
@@ -39,22 +42,26 @@ export class pageSearch extends LitElement{
             console.error('Error updating item:', error);  // Manejo de errores
         }
     }
+
     async deleteItem(id) {
         try {
-            await fetch(`https://664b70de35bbda10987cf5f7.mockapi.io/people/${id}`, {
-            method: 'DELETE'
+            await fetch(`https://664b70de35bbda10987cf5f7.mockapi.io/Inventory/${id}`, {
+                method: 'DELETE'
             });
             this.data = this.data.filter(item => item.id !== id);  // Actualizar la lista de datos eliminando el elemento
         } catch (error) {
             console.error('Error deleting item:', error);  // Manejo de errores
         }
     }
+    
+
     handleInputChange(e, type) {
         const { name, value } = e.target;
-        if (type === 'new') {
-            this.newItem = { ...this.newItem, [name]: value };  // Actualizar el nuevo elemento
-        } else if (type === 'edit') {
+        if (type === 'edit') {
             this.editItem = { ...this.editItem, [name]: value };  // Actualizar el elemento en edición
+        } else if (type === 'search') {
+            this.searchItem = value
+            this.requestUpdate();
         }
     }
 
@@ -63,48 +70,34 @@ export class pageSearch extends LitElement{
         this.editItem = { ...item };
     }
     
-
     render(){
 
         return html`
             <div>
                 <div class="search-bar">
-                    <label for="searcher">Type the ${zzz}'s id you're looking for!</label>
-                    <input type="text" id="searcher">
-                </div>
-                <ul>
-                    Results
-                    ${this.data[this.type].filter(item => item.value === this.pageData[this.tag], html`
-                    <li>
-                        ${bla} - ${bla} - ${bla} - ${bla} - ${bla}
-                        <button @click="${() => this.setEditItem(item)}">Editar</button>
-                        <button @click="${() => this.deleteItem(item.id)}">Eliminar</button>        
-                    </li>
-                    `)}
-                </ul>
-            
-                ${this.editItem ? html`
-                    <div>
-                    <h3>Editar Item</h3>
-                    <input name="name" .value="${this.editItem.name}" @input="${e => this.handleInputChange(e, 'edit')}" placeholder="Nombre" />
-                    <input name="city" .value="${this.editItem.city}" @input="${e => this.handleInputChange(e, 'edit')}" placeholder="Ciudad" />
-                    <button @click="${this.updateItem}">Actualizar</button>
-                    <button @click="${() => this.editItem = null}">Cancelar</button>
+                    <label for="searcher"><h2>Type the ${this.type}'s id you're looking for!</h2></label>
+                    <input type="text" id="searcher" @input="${e => this.handleInputChange(e, 'search')}">
                     </div>
-                ` : ''}  
-            </div>
+                    <h3>---- Results ----</h3>
+                    <ul>
+
+                    ${this.searchItem !== '' ? (this.data).filter(item => item.tag === this.searchItem).map(item => html`
+                    <li>
+                        <h4>${item.tag}</h4> - ${item.name} - ${item.stock}
+                        <button @click="${() => this.setEditItem(item)}">Editar</button>
+                        <button @click="${() => this.deleteItem(item.id)}">Eliminar</button>                     
+                    </li>
+                        `) : (this.data).map(item => html`
+                        <li>
+                        <h4>${item.tag}</h4> - ${item.name} - ${item.stock}
+                        <button @click="${() => this.setEditItem(item)}">Editar</button>
+                        <button @click="${() => this.deleteItem(item.id)}">Eliminar</button> 
+                        
+                        </li>`) }
+                        </ul>
+                        </div>
         `
     }
 }
 
-// async deleteItem(id) {
-//     try {
-//       await fetch(`https://664b70de35bbda10987cf5f7.mockapi.io/people/${id}`, {
-//         method: 'DELETE'
-//       });
-//       this.data = this.data.filter(item => item.id !== id);  // Actualizar la lista de datos eliminando el elemento
-//     } catch (error) {
-//       console.error('Error deleting item:', error);  // Manejo de errores
-//     }
-//   }
 customElements.define("page-search", pageSearch);
