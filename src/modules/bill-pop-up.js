7da98b684e30d45b826e7d609d7e3cbf8768aadf
AndreyJz <jerez.01.18@gmail.com>
2024-06-04 10:05:40 -0500

    styles: :lipstick: styles to bill

--- src/modules/bill-pop-up.js
+++ src/modules/bill-pop-up.js
@@ -1,6 +1,155 @@
 import { LitElement, css, html } from "lit";
 
 export class billPopUp extends LitElement{
+
+    static styles = css`
+
+        .grande{
+            width: 95%;
+            margin:2.5%;
+        }
+
+        .button-container {
+            gap: 1em;
+            margin: 2em;
+        }
+    
+        .back-button {
+            padding: 0;
+            margin: 0;
+            border: none;
+            background: none;
+            cursor: pointer;
+            --primary-color: #ffff;
+            --hovered-color: #ffff;
+            position: relative;
+            display: flex;
+            font-weight: 600;
+            font-size: 20px;
+            gap: 0.5rem;
+            align-items: center;
+        
+        }
+    
+        .back-button p {
+            margin: 0;
+            position: relative;
+            font-size: 20px;
+            color: var(--primary-color);
+        }
+    
+        .back-button::after {
+            position: absolute;
+            content: "";
+            width: 0;
+            right: 0;
+            bottom: -7px;
+            background: var(--hovered-color);
+            height: 2px;
+            transition: 0.3s ease-out;
+        }
+    
+        .back-button p::before {
+            position: absolute;
+            content: "Go back";
+            width: 0%;
+            inset: 0;
+            color: var(--hovered-color);
+            overflow: hidden;
+        }
+    
+        .back-button:hover::after {
+            width: 100%;
+            left: auto;
+            right: 0;
+        }
+    
+        .back-button:hover p::before {
+            width: 100%;
+        }
+    
+        .back-button:hover svg {
+            transform: translateX(-4px);
+            color: var(--hovered-color);
+        }
+    
+        .back-button svg {
+            color: var(--primary-color);
+            transition: 0.2s;
+            position: relative;
+            width: 15px;
+            transition-delay: 0.2s;
+        }
+    
+        .back-button-container {
+            align-self: center;
+        }
+    
+        .submit {
+            position: relative;
+            background-color: #007bff !important;
+            border-radius: 5px;
+            box-shadow: #012bff 0px 4px 0px 0px;
+            padding: 15px;
+            background-repeat: no-repeat;
+            cursor: pointer;
+            box-sizing: border-box;
+            width: 200px;
+            height: 49px;
+            color: #fff;
+            border: none;
+            font-size: 20px;
+            transition: all 0.3s ease-in-out;
+            z-index: 1;
+        
+        }
+    
+        .submit::before {
+            content: "";
+            background-color: #018bff !important;
+            width: 0;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            transition: width 700ms ease-in-out;
+            display: inline-block;
+        }
+    
+        .submit:hover::before {
+            width: 100%;
+        }
+
+        table {
+            border-collapse: collapse;
+            display: flex;
+            text-align: center; 
+        }
+
+        
+        
+        td, th {
+            border: 1px dotted #000;
+            padding: 8px;
+            text-align: left;
+            background-color: #fff;
+            color:#3c3c3c;
+        }
+        th {
+            background-color: #ccc;
+            color:#3c3c3c;
+        }
+        .materials{
+            font-size:8px;
+        }
+        @media (min-width: 600px) {
+            .materials{
+                font-size:15px;
+            }
+        }
+    `
+
     connectedCallback(){
         super.connectedCallback();
         const formPage = document.querySelector('page-form');
@@ -11,51 +160,56 @@ export class billPopUp extends LitElement{
     }
     render(){
         return html`
-            <h2>aaa</h2>
-        <div>
-            <hr>
+        <div class="button-container">
+            <button class="back-button">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="4" >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10 5L3 12m0 0l7 7m-7-7h18" ></path>
+                </svg>
+                <p>Go back</p>
+            </button>
+        </div>
+        <div class="grande">
+            
             <div>
-            <h3>Bill ID: ${this.billInfo['tag']}</h3>
+                <h3>Bill ID: ${this.billInfo['tag']}</h3>
                 
             </div>
-            <hr>
+            
             <div>
-            <h3>Productos a fabricar: ${this.billInfo['quantity']}</h3>
-                
+                <h3>Productos a fabricar: ${this.billInfo['quantity']}</h3>
             </div>
-            <hr>
+            
             <h2>Material's Info</h2>
             <div class="materials"> 
-            ${this.ShowMaterials()} 
+                ${this.ShowMaterials()} 
             </div>
-            <hr>
+            
             <h2>Efective Percent</h2>
             <div class="efective"> 
-            ${this.ShowEfective()}  
+                ${this.ShowEfective()}  
             </div>
-            <hr>
-            <h2>employees's Info</h2>
+            
+            <h2>Employees's Info</h2>
             <div class="employees">
-            ${this.ShowEmployees()}
+                ${this.ShowEmployees()}
             </div>
-            <hr>
-            <h2>indirectCost Info</h2>
+            
+            <h2>IndirectCost Info</h2>
             <div class="indirectCost">
-            ${this.ShowIndirectCost()}
+                ${this.ShowIndirectCost()}
             </div>
-            <hr>
+            
             <h2>Total Info</h2>
             <div class="total">
                 ${this.ShowTotal()}
             </div>
-            <hr>
+            
             <div class="notes">
                 ${this.ShowNotes()}
             </div>
         </div>
-        <div>
-        <button class="Back">Go back</button>
-        <button class="Send" disabled>Sent to data base</button>
+        <div class="button-container">
+            <button class="submit" disabled>Sent to data base</button>
         </div>
         `
     }
@@ -206,12 +360,11 @@ export class billPopUp extends LitElement{
         }
     };
 
- 
 
     firstUpdated(){
         this.isNote()
 
-        const backButton =this.shadowRoot.querySelector('.Back')
+        const backButton =this.shadowRoot.querySelector('.back-button')
         backButton.addEventListener('click',()=>{
 
             const goBack = `<page-new></page-new>`;
@@ -219,7 +372,7 @@ export class billPopUp extends LitElement{
             this.parentNode.removeChild(this);
         })
 
-        const sendButton =this.shadowRoot.querySelector('.Send')
+        const sendButton =this.shadowRoot.querySelector('.submit')
         const materialInfo=this.billInfo['product']
         const newReport =this.billInfo
         sendButton.addEventListener('click',async ()=>{
